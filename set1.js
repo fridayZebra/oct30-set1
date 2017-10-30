fbf42682a2d0b318d4d7482db65dc8155cf16043
Firoz Kamdar <firozkamdar@gmail.com>
2017-10-30 14:10:14 -0700

    drill 5 complete

--- set1.js
+++ set1.js
@@ -1,7 +1,15 @@
 'use strict';
+
+function yearOfBirth(age) {
+ return 2017 - age;
+}
+
 function whoAmI(name, age) {
-    let yearOfBirth = 2017 - age;
+    let yearOfBirth = 2017 - age; 
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-    console.log("I was born in " + yearOfBirth);
+    console.log("I was born in " + yearOfBirth(25));     
 };
-whoAmI('firoz', 25);
+
+
+whoAmI('john', 25);
+
