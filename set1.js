472e7b4e82140cc8ff19687a858cd46f0b57aa59
Bronson <keliiakimoto@gmail.com>
2017-10-30 15:04:05 -0700

    Drill 6 complete

--- set1.js
+++ set1.js
@@ -1,15 +1,23 @@
 'use strict';
 
-function yearOfBirth(age) {
- return 2017 - age;
-}
-
 function whoAmI(name, age) {
-    let yearOfBirth = 2017 - age; 
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-    console.log("I was born in " + yearOfBirth(25));     
+    console.log("I was born in " + yearOfBirth(age));
 };
 
+function yearOfBirth(age) {
+    if (name = undefined, age <= 0) {
+        throw new Error('Not a real person')
+    } 
+    else {   
+        return 2017 - age;
+    }
+}
 
-whoAmI('john', 25);
+whoAmI('john');
 
+// try {
+//     whoAmI('ted', -1)
+// } catch(e) {
+//     console.error(e)
+// }
