8ddbff24a2d54126987c30cae795f1374cfb5788
Bronson <keliiakimoto@gmail.com>
2017-10-30 13:39:58 -0700

    Added yearOfBirth

--- set1.js
+++ set1.js
@@ -1,5 +1,7 @@
 'use strict';
 function whoAmI(name, age) {
+    let yearOfBirth = 2017 - age;
     console.log(`Hi, my ${name} is Firoz and I'm ${age} years old`);
+    console.log("I was born in " + yearOfBirth);
 };
 whoAmI('firoz', 25);
